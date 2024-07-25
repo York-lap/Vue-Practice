8d84e26481efd60a59bc1127ba09c8c5a84711c3
york_code <york_code@163.com>
2024-07-25 09:29:54 +0800

    Login——add layout&input verify

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
 </script>
 
 <template>
-  App.vue
+  
   <router-view/>
 </template>
 
--- src/views/admin/login.vue
+++ src/views/admin/login.vue
@@ -1,17 +1,58 @@
 <script setup>
-    import { useAdminStore } from '@/stores/admin'
+    import '@/assets/admin/css/login.css'
+    import {User, Lock} from "@element-plus/icons-vue"
+    import { reactive,ref } from 'vue';
 
-    const adminstore = useAdminStore()
-    adminstore.save("york-lap", "1234", "2024")
-    console.log(adminstore.data)
+    const data = reactive({
+        name: '',
+        password: '',
+    })
+
+    const elFormRef = ref() // el-form ref
+    const rules = {
+        name:[
+            {required: true, message:'Input your account', trigger:'blur'},
+            {min:2, max:10, message:'Limit len[2-10]', trigger:'blur'}
+        ],
+        password: [
+            {required:true, message:'Input tour password', trigger:'blur'}
+        ]
+    }
+
+    // 测试
+    const login = ()=>{
+        console.log(data);
+
+        elFormRef.value.validate((valid, fields) => {
+            // console.log("valid:",valid,"fields:",fields)
+            if (!valid){
+                return
+            }
+        })
+        
+    }
 
 </script>
 
 <template>
-    <h3>Login</h3>
+    <div class="login">
+        <el-form :model="data" :rules="rules" ref="elFormRef">
+            <div class="title">
+                York-lap
+            </div>
+            <el-form-item prop="name">
+                <el-input :prefix-icon = "User" v-model="data.name"/>
+            </el-form-item>
 
-    <el-icon><User/></el-icon>
-    <el-button type="primary">Login</el-button>
+            <el-form-item prop="password">
+                <el-input show-password :prefix-icon = "Lock" v-model="data.password"/>
+            </el-form-item>
+
+            <el-form-item>
+                <el-button type="primary" @click="login">Login</el-button>
+            </el-form-item>
+        </el-form>
+    </div>
     
 </template>
 
